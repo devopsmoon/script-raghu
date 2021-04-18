347a56073227671963699910ae212f7f5728adaa
devopsmoon <md_syd01@yahoo.co.in>
2021-04-19 01:25:12 +0530

    modified line for nginx on frontend

--- frontend.sh
+++ frontend.sh
@@ -43,7 +43,9 @@ print1 "Finish.....!"
 cd /tmp
 print "Download source code"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-unzip /tmp/frontend.zip /usr/share/nginx/html/
+cd /usr/share/nginx/html/
+rm -rf *
+unzip /tmp/frontend.zip
 mv static/* .
 rmdir static
 rm -rf README.MD
