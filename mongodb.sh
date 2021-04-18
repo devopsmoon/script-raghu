40f7cff2440b226f6632d5e634c36c7c5f49f97b
devopsmoon <md_syd01@yahoo.co.in>
2021-04-19 03:54:11 +0530

    added commands

--- mongodb.sh
+++ mongodb.sh
@@ -39,7 +39,29 @@ catalogue)
   print1 "Finish....."
   ;;
 mongodb)
+  print "Updating system"
+  yum update -y
+  status_check
   print "Installing mongodb server"
+  echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+yum install mongodb-org -y
+sed -i 's/127.0.0.1/0.0.0.0/'   /etc/mongod.conf
+systemctl enable mongod
+systemctl start mongod
+print "Download the source code"
+curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+cd /tmp
+unzip /tmp/mongodb.zip
+mongo < catalogue.js
+mongo < users.js
+status_check
+print "Restarting mongod service"
+systemctl restart mongod
   print1 "Finish......"
   ;;
 *)
