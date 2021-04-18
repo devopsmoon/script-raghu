dc467f39847b326f12d208b5a3b2a06d9f89be83
devopsmoon <md_syd01@yahoo.co.in>
2021-04-19 00:53:37 +0530

    modified all line on frontend

--- frontend.sh
+++ frontend.sh
@@ -6,45 +6,45 @@
 user_id=$(id -u)
 case $user_id in
 0)
-  ;;
+;;
 *)
-  echo "\e[1;31mYou should be root user to perform this script\e[0m"
-  exit 1
-  ;;
+echo "\e[1;31mYou should be root user to perform this script\e[0m"
+exit 1
+;;
 esac
 print() {
-  echo "\e[1;32m***********************$1************************\e[0m"
+echo "\e[1;32m***********************$1************************\e[0m"
 }
 print1() {
-  echo "\e[1;33m***********************$1*************************\e[0m"
+echo "\e[1;33m***********************$1*************************\e[0m"
 }
 status_check() {
-  case $? in
-  0)
-  echo "\e[1;32mSUCCESS.....!\e[0m"
-  ;;
-  *)
-    echo "\e[1;31mFAILURE......!\e[0m"
-    exit 3
-    ;;
-    esac
+case $? in
+0)
+echo "\e[1;32mSUCCESS.....!\e[0m"
+;;
+*)
+echo "\e[1;31mFAILURE......!\e[0m"
+exit 3
+;;
+esac
 }
 case $1 in
 frontend)
-  print "Installing nginx server"
-  print1 "Finish......"
-  ;;
+print "Installing nginx server"
+print1 "Finish......"
+;;
 catalogue)
-  print "Installing nodejs server"
-  print1 "Finish......"
-  ;;
+print "Installing nodejs server"
+print1 "Finish......"
+;;
 mongodb)
-  print "Installing mongodb server"
-  print1 "Finish......."
-  ;;
+print "Installing mongodb server"
+print1 "Finish......."
+;;
 *)
-  echo "\e[1;31mInvalidate input provide the validate input\e[0m"
-  echo "\e[1;32mUsage: $0 | frontend | catalogue | mongodb\e[0m"
-  exit 2
-  ;;
+echo "\e[1;31mInvalidate input provide the validate input\e[0m"
+echo "\e[1;32mUsage: $0 | frontend | catalogue | mongodb\e[0m"
+exit 2
+;;
 esac
