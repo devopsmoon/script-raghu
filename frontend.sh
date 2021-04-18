88748dac3c0d018822cb033db2796605cca79492
devopsmoon <md_syd01@yahoo.co.in>
2021-04-19 01:19:19 +0530

    installing nginx on frontend

--- frontend.sh
+++ frontend.sh
@@ -32,7 +32,24 @@ esac
 case $1 in
 frontend)
 print "Installing nginx server"
-print1 "Finish......"
+yum update -y
+print1 "Finish......!"
+yum install nginx -y
+status_check
+print "Starting nginx service"
+systemctl enable nginx
+systemctl start nginx
+print1 "Finish.....!"
+cd /tmp
+print "Download source code"
+curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+unzip /tmp/frontend.zip /usr/share/nginx/html/
+mv static/* .
+rmdir static
+rm -rf README.MD
+print "Restarting nginx server"
+systemctl restart nginx
+print1 "Finish.....!"
 ;;
 catalogue)
 print "Installing nodejs server"
